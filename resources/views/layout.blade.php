8fdb8651579b3fa289e0b4505ac1d347e3e233f1
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-12 23:57:47 +0400

    chore: add script for language switch dropdown

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Corona Time</title>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         @vite('resources/css/app.css')
     </head>
     <body >
         <div>
+            <x-language-switch/>
             @yield('content')
         </div>
     </body>
