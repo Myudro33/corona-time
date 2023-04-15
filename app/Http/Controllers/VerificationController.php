cdba47b7d8d3c8a41759949932a40756e132e576
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-15 20:05:26 +0400

    feat: give access user to change password

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class VerificationController extends Controller
 {
@@ -29,10 +30,27 @@ class VerificationController extends Controller
 
         // Mark the user's email as verified
         $user->email_verified_at = now();
-        $user->verification_token = null;
+        $user->verification_token = Str::random(40);
         $user->save();
 
         // Redirect the user to the home page or wherever you want
         return redirect('/confirmed')->with('success', 'Your email has been verified!');
+    }
+
+
+
+    
+    public function ResetPassword(Request $request, $token)
+    {
+        // Find the user with the given token
+        $user = User::where('verification_token', $token)->first();
+        // If the user doesn't exist, return an error
+        if (!$user) {
+            return abort(404, 'User Not Found');
+        }
+        $user->verification_token = 'reset-verified';
+        $user->save();
+        // Redirect the user to the home page or wherever you want
+        return redirect("/password-update/$user->id");
     }
 }
