a65e1477bc14c82195f61e7acc4b9f8f1068a301
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-15 20:02:26 +0400

    feat: add reset passwrod controller

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PasswordUpdateRequest;
+use App\Mail\ResetPassword;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class ResetPasswordController extends Controller
+{
+    public function index()
+    {
+        return view('pages.forgot_password');
+    }
+    public function show(User $user)
+    {
+        if ($user->verification_token == 'reset-verified') {
+            return view('pages.password-update', [
+                'user' => $user,
+            ]);
+        } else {
+            return redirect('/confirmation');
+        }
+    }
+    public function view()
+    {
+     return view('pages.password-update-confirmed');
+    }
+
+    public function store(Request $request, User $user)
+    {
+        $user->password = bcrypt($request->password);
+        $user->verification_token = Str::random(40);
+        $user->save();
+        return redirect('/password-confirmed');
+    }
+    public function update(PasswordUpdateRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $mail = new ResetPassword($user);
+        $mail->setUser($user);
+        Mail::to($user->email)->send(new ResetPassword($user));
+        return redirect('/confirmation');
+    }
+}
