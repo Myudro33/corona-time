ca4019944a556f0bbbc5d588438c1ac4f165c9d2
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-15 15:09:09 +0400

    style: add register success page

---
+++ resources/views/pages/confirmation.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+<div class="w-screen h-screen flex flex-col xs:p-4 md:p-10">
+    <img class="xs:w-32 md:w-44 md:mx-auto" src="/assets/logo.png" alt="">
+    <div class="m-auto w-11/12 flex flex-col justify-center items-center">
+        <img class="w-14 h-14"  src="/assets/icon.gif" alt="">
+        <h1 class="mt-4 text-lg font-normal text-center">We have sent you a confirmation email</h1>
+    </div>
+</div>
+@endsection
