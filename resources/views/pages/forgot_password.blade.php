9a1e0f6c4afeb5876389852894db485143b5dee4
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-15 20:12:12 +0400

    add password update request

--- resources/views/pages/forgot_password.blade.php
+++ resources/views/pages/forgot_password.blade.php
@@ -1,16 +1,22 @@
 @extends('layout')
 @section('content')
     <div class="flex flex-col md:items-center w-full h-screen xs:py-6 xs:px-4 md:py-14">
-        <x-language-switch class="md:left-9" />
+        <x-language-switch class="md:left-10" />
         <img class="w-44 h-11" src="/assets/logo.png" alt="logo">
-        <form class="xs:h-screen xs:flex xs:flex-col xs:justify-between md:w-[392px] md:h-auto md:mt-36" action="#"
+        <form class="xs:h-screen xs:flex xs:flex-col xs:justify-between md:w-[392px] md:h-auto md:mt-36" action="{{{ route('reset.password') }}}"
             method="post">
+           @csrf
             <div>
                 <h1 class="text-[#010414] font-black xs:text-xl md:text-[25px] xs:my-10 md:my-14  text-center">
                     @lang('reset.heading')</h1>
                 <label class="text-[#010414] font-bold text-base mt-6" for="email">@lang('reset.email')</label>
-                <input class="w-full h-14 pl-6 py-4 border border-[#E6E6E7] rounded-lg my-2" type="email"
+                <input class="w-full h-14 pl-6 py-4 border border-[#E6E6E7] rounded-lg my-2" id="email" name="email" type="email"
                     placeholder="@lang('reset.email_placeholder')">
+                    @error('email')
+                        <p class="text-red-500 text-xs">
+                            {{$message}}
+                        </p>
+                    @enderror
             </div>
             <x-button class="md:mt-14" >@lang('reset.sign_up')</x-button>
         </form>
