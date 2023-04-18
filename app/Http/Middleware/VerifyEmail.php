7ce786c70b1ed47316db80eca6233eef7cd07de9
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-18 18:50:15 +0400

    fix: validate wrong password

--- app/Http/Middleware/VerifyEmail.php
+++ app/Http/Middleware/VerifyEmail.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class VerifyEmail
@@ -16,11 +17,18 @@ class VerifyEmail
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $password = $request->password;
         $user = User::where('email', $request->username)
             ->orWhere('username', $request->username)
             ->first();
-        if ($user && $user->email_verified_at === null) {
-            return redirect('/confirmation');
+        if ($user) {
+            if (Hash::check($password, $user->password)) {
+                if ($user->email_verified_at === null) {
+                    return redirect('/confirmation');
+                }
+            } else {
+                return back()->withErrors(['password' => __('login.wrong_password')]);
+            }
         }
         return $next($request);
     }
