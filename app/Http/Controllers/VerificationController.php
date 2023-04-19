be9962e3c2283373a20608cfb53ef35442e475d5
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-19 23:09:33 +0400

    refactor: user email verification function

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -21,25 +21,20 @@ class VerificationController extends Controller
     public function verifyEmail(Request $request, $token)
     {
         // Find the user with the given token
-        $user = User::where('verification_token', $token)->first();
-
         // If the user doesn't exist, return an error
-        if (!$user) {
-            return abort(404, 'User Not Found');
-        }
-
+        $user = User::where('verification_token', $token)->firstOrFail();
         // Mark the user's email as verified
         $user->email_verified_at = now();
         $user->verification_token = Str::random(40);
         $user->save();
 
         // Redirect the user to the home page or wherever you want
-        return redirect('/confirmed')->with('success', 'Your email has been verified!');
+        return redirect('/confirmed');
     }
 
 
 
-    
+
     public function ResetPassword(Request $request, $token)
     {
         // Find the user with the given token
