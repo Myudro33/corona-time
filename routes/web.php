821631dda650f2bc7c7a2afce6a945406aa4383e
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-19 23:48:23 +0400

    feat: add route names and middlewares

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,24 @@ Route::get('/locale/{lang}', [LanguageController::class, 'setLang']);
 
 // user
 Route::controller(AuthController::class)->group(function () {
-    Route::get('/', 'index')->middleware('auth')->name('dashboard');
+    Route::get('/', 'index')->name('dashboard')->middleware('auth');
     Route::get('/login', 'view')->name('login.view')->middleware('guest');
     Route::post('/login/user', 'login')->name('login')->middleware('verify.api');
-    Route::get('/register', 'create')->name('register.create');
-    Route::post('/register', 'register')->name('register');
+    Route::get('/register', 'create')->name('register.create')->middleware('guest');
+    Route::post('/register', 'register')->name('register')->middleware('guest');
     Route::post('/logout', 'logout')->name('logout')->middleware('auth');
 });
 Route::controller(VerificationController::class)->group(function () {
     Route::get('/verify-email/{token}', 'verifyEmail')->name('verification.verify');
     Route::get('/reset-password/{token}', 'ResetPassword')->name('verification.verify');
     Route::get('/confirmation', 'view')->name('confirmation')->name('verification.notice');
-    Route::get('/confirmed', 'show');
+    Route::get('/confirmed', 'show')->name('confirmed.show');
 });
 
 Route::controller(ResetPasswordController::class)->group(function () {
-    Route::get('/forgot-password', 'index');
-    Route::get('/password-update/{user}', 'show');
-    Route::get('/password-confirmed', 'view');
-    Route::post('/password-update/{user}', 'store')->name('password.store');
-    Route::post('/forgot-password', 'update')->name('password.update');
+    Route::get('/forgot-password', 'index')->name('forgot-password.index');
+    Route::get('/password-update/{user}', 'show')->name('password-update.show');
+    Route::get('/password-confirmed', 'view')->name('password-confirmed.view');
+    Route::post('/password-update/{user}', 'reset_password')->name('password.reset');
+    Route::post('/forgot-password', 'send_reset_password_mail')->name('password.email');
 });
