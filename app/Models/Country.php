5fc1f5debffae217f4fc5ed9bd100432fbf3efbb
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-21 17:55:18 +0400

    feat: create country model

---
+++ app/Models/Country.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Country extends Model
+{
+    use HasFactory;
+    use HasTranslations;
+    public $translatable = ['name'];
+    protected $fillable = ['country', 'code', 'confirmed', 'recovered', 'critical', 'deaths', 'name'];
+}
