8935bfe30e6d15f30261adade4aa7bc4d1edecc6
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-21 17:18:59 +0400

    wip: worldwide dashboard

--- resources/views/pages/worldwide.blade.php
+++ resources/views/pages/worldwide.blade.php
@@ -6,5 +6,13 @@
             <a class="border-b-4 border-black h-10" href="/worldwide">Worldwide</a>
             <a class="h-10" href="/bycountry">By country</a>
         </div>
+        <div class="flex xs:mt-6 md:mt-10 w-full md:h-auto xs:h-[420px] justify-between flex-wrap">
+            <x-worldwide-statistics class="xs:w-full xs:h-48" title="New cases" quantity="715123" background='#202bf315'
+                color='#2029F3' image='assets/blue.svg' />
+            <x-worldwide-statistics class="xs:w-[48%] xs:h-11/12" title="Recovered" quantity="432123" background='#0FBA6815'
+                color='#0FBA68' image='assets/green.svg' />
+            <x-worldwide-statistics class="xs:w-[48%] xs:h-11/12" title="Deaths" quantity="614243" background='#EAD62115'
+                color='#EAD621' image='assets/yellow.svg' />
+        </div>
     </x-dashboard>
 @endsection
