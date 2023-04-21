76700a273411b0fa7b0d00bcdd0b52f66236c1ae
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-21 17:18:34 +0400

    style: create dashboard card component

--- resources/views/components/worldwide-statistics.blade.php
+++ resources/views/components/worldwide-statistics.blade.php
@@ -1,7 +1,7 @@
 @props(['title', 'quantity', 'image', 'color', 'background'])
 
-<div class="h-full w-[392px] flex flex-col items-center bg-[{{ $background }}] rounded-2xl">
-    <img class="w-[90px] h-[64px] mt-10" src="{{ $image }}" alt="">
-    <h1 class="mt-6 font-medium text-xl text-[#010414]">{{ $title }}</h1>
-    <h1 class="text-[{{ $color }}] text-4xl font-black mt-4">{{ number_format($quantity) }}</h1>
+<div {{ $attributes(['class' =>'md:h-[255px] md:w-[392px] flex flex-col items-center rounded-2xl'])}} style="background-color: {{ $background }}">
+    <img class="w-[90px] h-[64px] md:mt-10 xs:mt-2" src="{{ $image }}" alt="">
+    <h1 class="mt-6 font-medium md:text-xl xs:text-base text-[#010414]">{{ $title }}</h1>
+    <h1 style="color: {{ $color }}" class="md:text-4xl xs:text-2xl font-black mt-4">{{ number_format($quantity) }}</h1>
 </div>
