fa008dd87af10d4fd68bb5112728b88774238af7
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-21 15:45:48 +0400

    style: fix language switch component

--- resources/views/components/language-switch.blade.php
+++ resources/views/components/language-switch.blade.php
@@ -2,9 +2,11 @@
     $lang = app()->getLocale();
 @endphp
 
-<div {{ $attributes(['class' => 'w-24 text-center absolute md:left-[45%] xs:right-5 top-6 p-2 rounded-md bg-gray-200']) }}
+<div {{ $attributes(['class' => 'w-28 text-center absolute md:left-[45%] xs:right-5 top-6 p-2 rounded-md bg-white shadow-md']) }}
     x-data="{ show: false }">
-    <button @click="show = !show">{{ $lang == 'en' ? 'English' : 'ქართული' }}</button>
+    <button class="flex items-center" @click="show = !show">{{ $lang == 'en' ? 'English' : 'ქართული' }} <img class="mx-1"
+            src="/assets/stroke.svg" alt="">
+    </button>
     <div style="display: none" x-show="show">
         <a class="block" href="/locale/en">English</a>
         <a class="block" href="/locale/ka">ქართული</a>
