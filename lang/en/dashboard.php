f559e752804fe6463b5442548ba12956f4c71b8f
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-22 18:09:53 +0400

    feat: dashboard localization

---
+++ lang/en/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'logout'=>'Log Out',
+    'heading_title'=>'Worldwide Statistics',
+    'worldwide'=>'Worldwide',
+    'by_country'=> 'By country',
+    'new_cases'=>'New cases',
+    'recovered'=>'Recovered',
+    'deaths'=>'Deaths',
+    'input_placeholder'=>'Search by country',
+    'location'=>'Location'
+];
