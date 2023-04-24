64bd3321d0e233edd36ca83ea04f2127b9a96198
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-24 14:45:22 +0400

    feat: add country search query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,21 +19,29 @@ class DashboardController extends Controller
     }
     public function show(Request $request): View
     {
+        $lang = app()->getLocale();
         $sort = $request->input('sort');
         $order = $request->input('order');
         $nextOrder = $order === 'asc' ? 'desc' : 'asc';
         $countries = Country::query();
-        if($sort) {
+        if ($sort) {
             $countries->orderBy($sort, $order);
         }
+        if (request('search')) {
+            $countries = DB::table('countries')->where(DB::raw("json_extract(name, '$. " . "$lang')"), 'LIKE', '%' . ucfirst(request('search')) . '%');
+        }
         $countries = $countries->get();
-        return view('pages.by-country', [
-            'confirmed' => DB::table('countries')->sum('confirmed'),
-            'recovered' => DB::table('countries')->sum('recovered'),
-            'deaths' => DB::table('countries')->sum('deaths'),
-            'order'=>$order,
-            'column'=>$sort,
-            'nextOrder' => $nextOrder,
-        ], compact('countries'));
+        return view(
+            'pages.by-country',
+            [
+                'confirmed' => DB::table('countries')->sum('confirmed'),
+                'recovered' => DB::table('countries')->sum('recovered'),
+                'deaths' => DB::table('countries')->sum('deaths'),
+                'order' => $order,
+                'column' => $sort,
+                'nextOrder' => $nextOrder,
+            ],
+            compact('countries'),
+        );
     }
 }
