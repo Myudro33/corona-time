003d6af7fa1dabdbec70d24d95b414a7a2221870
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-25 12:41:05 +0400

    refactor: move sorting and searching function in country model

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,37 +17,13 @@ class DashboardController extends Controller
             'deaths' => DB::table('countries')->sum('deaths'),
         ]);
     }
-    public function show(Request $request): View
+    public function show(): View
     {
-        $lang = app()->getLocale();
-        $sort = $request->input('sort');
-        $order = $request->input('order');
-        $nextOrder = $order === 'asc' ? 'desc' : 'asc';
-        $countries = Country::query();
-        if (request('search')) {
-            $countries = DB::table('countries')
-                ->where(DB::raw("json_extract(name, '$. " . "$lang')"), 'LIKE', '%' . request('search') . '%')
-                ->orWhere(DB::raw("json_extract(name, '$. " . "$lang')"), 'LIKE', '%' . ucfirst(request('search')) . '%');
-        }
-        if ($sort) {
-            if ($sort == 'name') {
-                $countries->orderByRaw("CAST(JSON_EXTRACT(name, '$." . $lang . "') AS CHAR) " . $order)->orderBy($sort, $order);
-            } else {
-                $countries->orderBy($sort, $order);
-            }
-        }
-        $countries = $countries->get();
-        return view(
-            'pages.by-country',
-            [
-                'confirmed' => DB::table('countries')->sum('confirmed'),
-                'recovered' => DB::table('countries')->sum('recovered'),
-                'deaths' => DB::table('countries')->sum('deaths'),
-                'order' => $order,
-                'column' => $sort,
-                'nextOrder' => $nextOrder,
-            ],
-            compact('countries'),
-        );
+        return view('pages.by-country', [
+            'confirmed' => DB::table('countries')->sum('confirmed'),
+            'recovered' => DB::table('countries')->sum('recovered'),
+            'deaths' => DB::table('countries')->sum('deaths'),
+            'countries' => Country::filter()->get(),
+        ]);
     }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Spatie\Translatable\HasTranslations;
 
 class Country extends Model
@@ -12,4 +13,23 @@ class Country extends Model
     use HasTranslations;
     public $translatable = ['name'];
     protected $fillable = ['country', 'code', 'confirmed', 'recovered', 'critical', 'deaths', 'name'];
+
+    public function scopeFilter()
+    {
+        $lang = app()->getLocale();
+        $countries = Country::query();
+        if (request('search')) {
+            $countries = DB::table('countries')
+                ->where(DB::raw("json_extract(name, '$. " . "$lang')"), 'LIKE', '%' . request('search') . '%')
+                ->orWhere(DB::raw("json_extract(name, '$. " . "$lang')"), 'LIKE', '%' . ucfirst(request('search')) . '%');
+        }
+        if (request('sort')) {
+            if (request('sort') == 'name') {
+                $countries->orderByRaw("CAST(JSON_EXTRACT(name, '$." . $lang . "') AS CHAR) " . request('order'))->orderBy(request('sort'), request('order'));
+            } else {
+                $countries->orderBy(request('sort'), request('order'));
+            }
+        }
+        return $countries;
+    }
 }
