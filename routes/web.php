b305e9784c0f593b733870cf46822a84bb4cac0d
Myudro33 <nika.kanashvili@redberry.ge>
2023-04-26 14:45:29 +0400

    fix: change middleware

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/locale/{lang}', [LanguageController::class, 'setLang']);
 Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'index')->name('dashboard')->middleware('auth');
     Route::get('/login', 'view')->name('login.view')->middleware('guest');
-    Route::post('/login', 'login')->name('login')->middleware('verify.api');
+    Route::post('/login', 'login')->name('login')->middleware('verify');
     Route::get('/register', 'create')->name('register.create')->middleware('guest');
     Route::post('/register', 'register')->name('register')->middleware('guest');
     Route::post('/logout', 'logout')->name('logout')->middleware('auth');
