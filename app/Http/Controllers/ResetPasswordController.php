66f2a363e3c6b2b01e02647867c35a528423ad21
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-03 14:44:54 +0400

    refactor: add validation in form request to check if this email already exists in reset tokens table

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -32,18 +32,11 @@ class ResetPasswordController extends Controller
     public function send_reset_password_mail(PasswordUpdateRequest $request): RedirectResponse
     {
         $token = Str::random(40);
-        $isExist = DB::table('password_reset_tokens')
-            ->where('email', $request->validated()['email'])
-            ->exists();
-        if (!$isExist) {
             DB::table('password_reset_tokens')->insert([
                 'email' => $request->validated()['email'],
                 'token' => $token,
                 'created_at' => now(),
             ]);
-        } else {
-            return back()->withErrors(['email' => __('passwords.email_already_sent')]);
-        }
         Mail::send('email.email-body-password', ['token' => $token], function ($message) use ($request) {
             $message->to($request->validated()['email']);
             $message->subject('Password Reset');
--- app/Http/Requests/PasswordUpdateRequest.php
+++ app/Http/Requests/PasswordUpdateRequest.php
@@ -6,11 +6,10 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PasswordUpdateRequest extends FormRequest
 {
-
     public function rules(): array
     {
         return [
-            'email'=>'required|email|exists:users,email'
+            'email'=>'required|email|exists:users,email|unique:password_reset_tokens,email'
         ];
     }
 }
