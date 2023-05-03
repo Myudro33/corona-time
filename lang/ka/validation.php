56fdd8a069d0815c9d2492ea45e216414b9416df
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-03 11:25:27 +0400

    fix: delete Apostrophes in error messages

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'email' => ":attribute'ს ველი უნდა იყოს იმეილის ფორმატის.",
+    'email' => ":attributeს ველი უნდა იყოს იმეილის ფორმატის.",
     'exists' => 'მითითებული :attribute არასწორია.',
     'min' => [
         'string' => ':attribute უნდა იყოს მინიმუმ :min ასო.',
     ],
-    'required' => ":attribute'ს ველი სავალდებულოა.",
-    'same' => ":attribute'ს ველი უნდა ემთხვეოდეს :other'ს ველს.",
+    'required' => ":attributeს ველი სავალდებულოა.",
+    'same' => ":attributeს ველი უნდა ემთხვეოდეს :otherს ველს.",
     'unique' => 'ასეთი :attribute უკვე არსებობს',
     'attributes' => [
         'username'=>'მომხმარებლის სახელი',
